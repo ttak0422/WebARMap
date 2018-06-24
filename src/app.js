384be227267e4db7740fb7019248ad289d5914fa
TakagiTakuro <bgdaewalkman@gmail.com>
2018-06-24 15:43:46 +0900

    add tiny function(reticle)

--- src/app.js
+++ src/app.js
@@ -1,9 +1,84 @@
-import main, { init } from './modules/main'
+import 'three/VRControls';
+
+
+/// ********* AR ********* ///
+var vrFrameData,vrDisplay, vrControls, arView; 
+var canvas, camera, scene, renderer, reticle;
+/// ********* -- ********* ///
+
+
+/// ********* AR ********* ///
+var reticle;
+/// ********* -- ********* ///
+
 
 THREEAR.ARUtils.getARDisplay().then(function (display) {
     if (display) {
-        init();
+        vrFrameData = new VRFrameData();
+        vrDisplay = display;
+        init();        
     } else {        
         THREEAR.ARUtils.displayUnsupportedMessage();
     }
 });
+
+
+function init() {
+    initArSystem();
+    initReticle();
+    update();
+}
+
+
+function update() {
+    updateArSystem();
+    updateReticle();
+}
+
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+
+function initArSystem() {
+    renderer = new THREE.WebGLRenderer({alpha: true});
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.autoClear = false;    
+    scene  = new THREE.Scene();
+    arView = new THREEAR.ARView(vrDisplay, renderer);
+    camera = new THREEAR.ARPerspectiveCamera(
+                vrDisplay,
+                60, //fov
+                window.innerWidth / window.innerHeight,
+                vrDisplay.depthNear,
+                vrDisplay.depthFar
+            );    
+    vrControls = new THREE.VRControls(camera);    
+    canvas = renderer.domElement;
+    document.body.appendChild(canvas);    
+}
+
+
+function updateArSystem() {
+    camera.updateProjectionMatrix();
+    vrDisplay.getFrameData(vrFrameData);
+    vrControls.update();
+    arView.render();    
+    renderer.clearDepth();    
+    renderer.render(scene, camera);
+    vrDisplay.requestAnimationFrame(update);
+}
+
+
+function initReticle() { 
+    reticle = new THREEAR.ARReticle(vrDisplay, 0.03, 0.04, 0xff0077, 0.25); 
+    scene.add(reticle);
+}
+
+function updateReticle() { 
+    reticle.update(0.5, 0.5); 
+}
