97ddf20c1db40ab03e33866631414b29d8b39ee9
TakagiTakuro <bgdaewalkman@gmail.com>
2018-06-26 03:05:55 +0900

    add 2d gui

--- src/app.js
+++ src/app.js
@@ -1,24 +1,26 @@
 import 'three/VRControls';
-
+import 'three/GLTFLoader';
 
 /// ********* AR ********* ///
 var vrFrameData,vrDisplay, vrControls, arView; 
+var curPos;
 var canvas, camera, scene, renderer, reticle;
 /// ********* -- ********* ///
-
 
 /// ********* other ********* ///
 var arDebugger;
 var reticle;
-
+var hudCanvas, hudBitmap, hudCamera, hudTexture, hudMaterial;
+var planeGeometry, plane, hudScene;
 /// ********* ----- ********* ///
 
 
 THREEAR.ARUtils.getARDisplay().then(function (display) {
-    if (display) {
+    if (display) {        
         vrFrameData = new VRFrameData();
-        vrDisplay = display;
-        init();        
+        vrDisplay = display;        
+        init();   
+        alert('ようこそARの世界へ！');
     } else {        
         THREEAR.ARUtils.displayUnsupportedMessage();
     }
@@ -29,6 +31,7 @@ function init() {
     initArSystem();
     initReticle();
     initDebugger();
+    initHud();    
     update();
 }
 
@@ -36,6 +39,7 @@ function init() {
 function update() {
     updateArSystem();
     updateReticle();
+    updateHud();
 }
 
 
@@ -46,6 +50,7 @@ function onWindowResize() {
 }
 
 
+/// ********* AR System ********* ///
 function initArSystem() {
     renderer = new THREE.WebGLRenderer({alpha: true});
     renderer.setPixelRatio(window.devicePixelRatio);
@@ -61,31 +66,65 @@ function initArSystem() {
                 vrDisplay.depthFar
             );    
     vrControls = new THREE.VRControls(camera);    
-    canvas = renderer.domElement;
-    document.body.appendChild(canvas);    
+    document.body.appendChild(renderer.domElement);
 }
-
-
 function updateArSystem() {
     camera.updateProjectionMatrix();
     vrDisplay.getFrameData(vrFrameData);
+    curPos = vrFrameData.pose.position;
     vrControls.update();
     arView.render();    
     renderer.clearDepth();    
-    renderer.render(scene, camera);
+    renderer.render(scene, camera);        
     vrDisplay.requestAnimationFrame(update);
 }
+/// ********* _________ ********* ///
 
+/// ********* AR Debugger ********* ///
 function initDebugger() {
     arDebugger = new THREEAR.ARDebug(vrDisplay);
     document.body.appendChild(arDebugger.getElement());
 }
+/// ********* ___________ ********* ///
 
+/// ********* Reticle ********* ///
 function initReticle() { 
     reticle = new THREEAR.ARReticle(vrDisplay, 0.03, 0.04, 0xff0077, 0.25); 
     scene.add(reticle);
 }
-
 function updateReticle() { 
     reticle.update(0.5, 0.5); 
 }
+/// ********* _______ ********* ///
+
+/// ********* HUD ********* ///
+function initHud() {
+    let width  = window.innerWidth;
+    let height = window.innerHeight;    
+    hudCanvas  = document.createElement('canvas');
+    hudCanvas.width  = width;
+    hudCanvas.height = height;
+    hudBitmap = hudCanvas.getContext('2d');
+    hudBitmap.font = "Normal 40px Arial";
+    hudBitmap.textAlign = 'center';
+    hudBitmap.fillStyle = 'rgba(45, 45, 45, 0.9)';
+    hudBitmap.fillText('Initializing...', width / 2, height / 2);
+    hudCamera  = new THREE.OrthographicCamera(-width/2, width/2, height/2, -height/2, 0, 30);
+    hudScene   = new THREE.Scene();
+    hudTexture = new THREE.Texture(hudCanvas);
+    hudTexture.needsUpdate = true;
+    hudMaterial = new THREE.MeshBasicMaterial({map: hudTexture});
+    hudMaterial.transparent = true; //重そう
+    planeGeometry = new THREE.PlaneGeometry(width, height);
+    plane = new THREE.Mesh(planeGeometry, hudMaterial);
+    hudScene.add(plane);  
+}
+var counter = 0; //フレーム数(動作確認用)
+function updateHud() {       
+    hudBitmap.clearRect(0, 0, window.innerWidth, window.innerHeight);
+    hudBitmap.fillText("counter : " + counter.toString(), window.innerWidth/2, window.innerHeight/2);
+    hudTexture.needsUpdate = true;
+    counter++;
+    renderer.render(hudScene, hudCamera);
+}
+/// ********* ___ ********* ///
