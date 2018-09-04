5c8e3ba6f2547e28a09d30c3cedf418ed6b21f80
TakagiTakuro <bgdaewalkman@gmail.com>
2018-09-05 04:33:09 +0900

    [fix] 文字全体の表示が可能になった

--- src/modules/ARSystem.js
+++ src/modules/ARSystem.js
@@ -143,12 +143,12 @@ module.exports = ARSystem = function(scene, cam, callback){
         const lines      = text.split("\n");
         const lineHeight = 1.1618; // 経験と実績から...
         const lineWidth  = Math.max.apply(null, lines.map(x => context.measureText(x).width));
-        // const charInLine = Math.max.apply(null, lines.map(x => x.length));
+        const charInLine = Math.max.apply(null, lines.map(x => x.length));
 
         console.log(`canvas:${canvas} context:${context} line:${lines} lineHeight:${lineHeight} lineWidth:${lineWidth}`);
         context.font = textSize + "px Arial";
 
-        const canvasWidth  = lineWidth + bgMargine * 2;
+        const canvasWidth  = charInLine * textSize + bgMargine * 2;
         const canvasHeight = textSize * lines.length * lineHeight + bgMargine * 2;
 
         console.log(`canvasWidth:${canvasWidth} canvasHeight:${canvasHeight}`);
